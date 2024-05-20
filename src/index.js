8faa63c296fb24b886177c6de7451ad843a1b66c
Jessica Espejo <jenespejoto@unal.edu.co>
2024-05-19 22:07:18 -0500

    Configuracion de express y port

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hola mundo');
+/* -------------------------- import dependencies ------------------------- */
+import express from 'express'
+
+
+/* ----------------------------- initialization ----------------------------- */
+const app = express();
+
+
+/* --------------------------------- setting (server and templates) -------------------------------- */
+app.set('port', process.env.PORT || 3000)
+
+/* ------------------------------- middlewares ------------------------------- */
+
+
+
+/* --------------------------------- routes --------------------------------- */
+
+/* ------------------------------ public files (access)------------------------------ */
+
+
+/* ------------------------------- run server (turn on the server)------------------------------- */
+app.listen(app.get('port'), () => {
+    console.log('server listening on port', app.get('port'));
+});
