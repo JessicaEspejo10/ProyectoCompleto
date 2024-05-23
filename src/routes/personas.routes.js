2bbdc58d4b97601ff30c16ae0e6473cfa6da13b5
Jessica Espejo <jenespejoto@unal.edu.co>
2024-05-22 20:42:00 -0500

    configuracion add

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -3,6 +3,25 @@ import pool from '../database.js' ;
 
 const router = Router();
 
+router.get('/add', (req,res) => {
+    res.render('personas/add');
+});
+
+router.post('/add', async(req,res) => {
+    try{
+        const {name,lastname,age} = req.body;
+        const newPersona = {
+            name, lastname,age
+        }
+        await pool.query('INSERT INTO personas SET ?', [newPersona]);
+        res.redirect('/list');
+
+
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
+
 router.get('/list',async(req,res) => {
     try{
         const [result] = await pool.query('SELECT * FROM personas');
@@ -12,4 +31,13 @@ router.get('/list',async(req,res) => {
     }
 });
 
+router.get('/delete/:id', async(req,res) => {
+    try{
+        const {id} = req.params
+        await pool.query('DELETE FROM personas WHERE id=?', [id]);
+        res.redirect('/list');
+    }catch(error){
+        res.status(500).json({message: error.message});
+    };
+});
 export default router;
