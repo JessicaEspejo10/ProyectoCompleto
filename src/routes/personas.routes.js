2ef3e4c73a2e13db3e0c5515f2e312934baa398d
Jessica Espejo <jenespejoto@unal.edu.co>
2024-05-27 11:18:13 -0500

    configuracion de imagenes en el formulario, rutas y vistas

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -1,21 +1,47 @@
 import {Router} from 'express'
 import pool from '../database.js' ;
+import multer from 'multer';
+import path from 'path';
 
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: 'src/public/uploads/',
+    filename: (req,file,cb) => {
+        const uniqueSuffix = Date.now() + '-' +  Math.round(Math.random() * 1E9);
+        const ext = path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    }
+});
+
+const upload = multer({storage});
 
 router.get('/add', (req,res) => {
     res.render('personas/add');
 });
 
-router.post('/add', async(req,res) => {
+//segundo parametro es una middleware
+router.post('/add', upload.single('file'), async(req,res) => {
     try{
         const {name,lastname,age,detail} = req.body;
-        const newPersona = {
-            name, lastname,age,detail
+        let newPersona={};
+
+        if(req.file){
+            const file = req.file;   
+            const original_imgname = file.originalname;           
+            const image = file.filename;
+
+            newPersona = {
+                name, lastname,age,detail,image
+            }
+        }else{
+            newPersona = {
+                name, lastname,age,detail
+            }
         }
+        
         await pool.query('INSERT INTO personas SET ?', [newPersona]);
         res.redirect('/list');
-
 
     }catch(error){
         res.status(500).json({message: error.message});
@@ -53,14 +79,28 @@ router.get('/edit/:id', async(req,res) => {
     };
 });
 
-router.post('/edit/:id', async(req,res) => {
+router.post('/edit/:id', upload.single('file'), async(req,res) => {
     try{
         const{id} = req.params
         const{name,lastname,age,detail} = req.body
-        const editPersona = {name,lastname,age,detail}
+        let editPersona = {}
+
+        if(req.file){
+            const file = req.file;   
+            const original_imgname = file.originalname;           
+            const image = file.filename;
+
+            editPersona = {
+                name, lastname,age,detail,image
+            }
+        }else{
+            editPersona = {
+                name, lastname,age,detail
+            }
+        }
+
         await pool.query('UPDATE personas SET ? WHERE id = ?', [editPersona,id]);
         res.redirect('/list');
-
 
     }catch(error){
         res.status(500).json({message: error.message});
